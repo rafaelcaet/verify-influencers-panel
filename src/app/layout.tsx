595fd04a8f9a19419e1d347efdcf8a9ab04d7bba
rafaelcaet <fael.caetsantos@gmail.com>
2025-01-07 20:12:39 -0300

    feat: added theme mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/NavBar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans bg-background`}
       >
-        <NavBar />
-        {children}
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { BrandLogo } from "./BrandLogo";
+import { SunMoon } from "lucide-react";
+import { ModeToggle } from "./ToggleButton/ToggleMode";
 
 export function NavBar() {
   return (
     <header className="flex py-6 shadow-lg fixed top-0 w-full z-10 dark:text-white text-black bg-background/95">
       <nav className="flex items-center gap-10 container font-medium">
-        <Link href="/" className="flex gap-3 m-auto">
+        <Link href="/" className="flex gap-3 mr-auto">
           <BrandLogo />
         </Link>
         <Link className="text-lg" href="/#leaderboards">
@@ -27,6 +29,9 @@ export function NavBar() {
           Admin
         </Link>
         <span className="text-lg">Sign out</span>
+        <div className="ml-10">
+          <ModeToggle />
+        </div>
       </nav>
     </header>
   );
