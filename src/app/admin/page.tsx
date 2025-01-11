7c877b2132021a9d59e52504713a54fc4f1f1b79
rafaelcaet <fael.caetsantos@gmail.com>
2025-01-11 11:32:04 -0300

    fix: improve background color and hover buttons

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -28,7 +28,7 @@ export default function Admin() {
           </CardHeader>
           <CardContent>
             <div className="flex gap-4 justify-center">
-              <Card className="dark:hover:bg-green-300/20 dark:hover:border-green-400 dark:bg-[#171717] px-24">
+              <Card className="dark:hover:bg-green-300/20 cursor-pointer dark:hover:border-green-400 hover:border-green-400 hover:bg-green-100 dark:bg-[#171717] px-24">
                 <CardContent className="flex flex-col items-center justify-center mt-5">
                   <p className="font-semibold">Specific Influencer</p>
                   <span className="text-sm text-gray-500">
@@ -37,7 +37,7 @@ export default function Admin() {
                   </span>
                 </CardContent>
               </Card>
-              <Card className="dark:hover:bg-green-300/20 dark:hover:border-green-400 dark:bg-[#171717] px-24">
+              <Card className="dark:hover:bg-green-300/20 cursor-pointer hover:border-green-400 hover:bg-green-100 dark:hover:border-green-400 dark:bg-[#171717] px-24">
                 <CardContent className="flex flex-col items-center justify-center mt-5">
                   <p className="font-semibold">Discovery New</p>
                   <span className="text-sm text-gray-500">
@@ -50,16 +50,16 @@ export default function Admin() {
             <div className="flex flex-col pl-9 max-w-[565px] mt-5 gap-1">
               <p className="p-1 text-sm font-light">Time Range</p>
               <div className="grid grid-cols-2 gap-2">
-                <Card className=" flex dark:hover:bg-green-300/20 dark:hover:border-green-400 dark:hover:text-green-400 dark:bg-[#171717] items-center justify-center p-2">
+                <Card className=" flex dark:hover:bg-green-300/20 cursor-pointer hover:border-green-400 hover:bg-green-100 dark:hover:border-green-400 dark:hover:text-green-400 hover:text-green-500 dark:bg-[#171717] items-center justify-center p-2">
                   <p className=" text-sm">Last Week</p>
                 </Card>
-                <Card className=" flex dark:hover:bg-green-300/20 dark:hover:border-green-400 dark:hover:text-green-400 dark:bg-[#171717] items-center justify-center">
+                <Card className=" flex dark:hover:bg-green-300/20 cursor-pointer hover:border-green-400 hover:bg-green-100 dark:hover:border-green-400 dark:hover:text-green-400 hover:text-green-500 dark:bg-[#171717] items-center justify-center">
                   <p className=" text-sm">Last Month</p>
                 </Card>
-                <Card className=" flex dark:hover:bg-green-300/20 dark:hover:border-green-400 dark:hover:text-green-400 dark:bg-[#171717] items-center justify-center p-2">
+                <Card className=" flex dark:hover:bg-green-300/20 cursor-pointer hover:border-green-400 hover:bg-green-100 dark:hover:border-green- hover:text-green-500 dark:hover:text-green-400 dark:bg-[#171717] items-center justify-center p-2">
                   <p className=" text-sm">Last Year</p>
                 </Card>
-                <Card className=" flex dark:hover:bg-green-300/20 dark:hover:border-green-400 dark:hover:text-green-400 dark:bg-[#171717] items-center justify-center">
+                <Card className=" flex dark:hover:bg-green-300/20 cursor-pointer hover:border-green-400 hover:bg-green-100 dark:hover:border-green-400 dark:hover:text-green-400 hover:text-green-500 dark:bg-[#171717] items-center justify-center">
                   <p className=" text-sm">All Time</p>
                 </Card>
               </div>
